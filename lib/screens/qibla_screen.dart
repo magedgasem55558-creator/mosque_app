95b13be325b776c76850160dc78949a1da7e71d5
magedgasem55558-creator <magedgasem55558@gmail.com>
2026-08-09 20:10:36 +0300

    Update qibla_screen.dart

--- lib/screens/qibla_screen.dart
+++ lib/screens/qibla_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import 'package:flutter_compass/flutter_compass.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
 
